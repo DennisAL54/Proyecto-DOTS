02b31b202902c8aecfe0b1f05d5ef52ce2889b86
Unknown <alejimenezc@hotmail.com>
2018-09-21 20:52:00 -0600

    Creación de la lista de listas

--- JavaApplication3/src/Proyecto/Server.java
+++ JavaApplication3/src/Proyecto/Server.java
@@ -5,17 +5,37 @@
  */
 package Proyecto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author aleji
  */
 public class Server {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    private boolean turno;
     
+    public void mainConversor(){
+        List<List> Botones;
+        List<Integer> Fila1;
+        List<Integer> Fila2;
+        List<Integer> Fila3;
+        Botones = new ArrayList<>();
+        Fila1 = new ArrayList<>();
+        Fila2 = new ArrayList<>();
+        Fila3 = new ArrayList<>();
+        Fila1.add(1);
+        Fila1.add(2);
+        Fila1.add(3);
+        Fila2.add(4);
+        Fila2.add(5);
+        Fila2.add(6);
+        Fila3.add(7);
+        Fila3.add(8);
+        Fila3.add(9);
+        Botones.add(Fila1);
+        Botones.add(Fila2);
+        Botones.add(Fila3);
+        System.out.println(Botones);
+    }
 }
