545af1f54c30a2d3fd6653a73e3a8e5b7d9d13b7
Roy Chavarría <roychav99@gmail.com>
2018-09-21 22:37:39 -0600

    prueba server socket

--- JavaApplication3/src/Proyecto/Main.java
+++ JavaApplication3/src/Proyecto/Main.java
@@ -13,5 +13,8 @@ public class Main {
     public static void main(String [] args){
         Server su = new Server();
         su.mainConversor();
+        
+        socket c = new socket();
+        c.iniciar();
     }
 }
