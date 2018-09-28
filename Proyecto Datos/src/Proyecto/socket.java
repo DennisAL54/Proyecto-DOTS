eeceb8f10984afaffcd37b1eb0be1b141846ef2e
Unknown <alejimenezc@hotmail.com>
2018-09-27 22:56:43 -0600

    Cambio al nombre de la carpeta

---
+++ Proyecto Datos/src/Proyecto/socket.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto;
+
+import java.net.*;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+/**
+ *
+ * @author roy
+ */
+public class socket implements Runnable{//Alternativa de socket
+    
+    
+    //Variables
+    ServerSocket server = null;
+    Socket socket;
+    int puerto = 9000;
+    Thread running = null;
+    boolean parado = false;
+    
+    //Establece el puerto
+
+    /**
+     *
+     * @param puerto
+     */
+   public socket (int puerto){
+        this.puerto = puerto;
+    }
+       
+   public void run(){
+       synchronized(this){
+       this.running = Thread.currentThread();
+   }
+    
+       
+       
+   }   
+
+    
+    
+}
